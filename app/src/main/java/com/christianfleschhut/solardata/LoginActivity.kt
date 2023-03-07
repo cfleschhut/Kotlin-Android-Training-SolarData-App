2492f828cff827f853e1d5b7aca420d6b86129ea
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-03-07 08:39:55 +0100

    Add basic login form validation

--- app/src/main/java/com/christianfleschhut/solardata/LoginActivity.kt
+++ app/src/main/java/com/christianfleschhut/solardata/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.christianfleschhut.solardata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.christianfleschhut.solardata.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -16,8 +17,15 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val inputEmail = binding.etEmail
+            val inputPassword = binding.etPassword
+
+            if (inputEmail.text!!.isNotEmpty() && inputPassword.text!!.isNotEmpty()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please enter login data", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
