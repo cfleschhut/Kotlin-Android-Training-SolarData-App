2d5b138a0cc0555c882b583549690dffaada169f
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-03-14 13:38:04 +0100

    Format x-axis values and entry labels in chart

--- app/src/main/java/com/christianfleschhut/solardata/Chart.kt
+++ app/src/main/java/com/christianfleschhut/solardata/Chart.kt
@@ -2,9 +2,28 @@ package com.christianfleschhut.solardata
 
 import com.christianfleschhut.solardata.data.Measurement
 import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.*
-import kotlin.random.Random
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+
+class Formatter : ValueFormatter() {
+
+    override fun getBarLabel(barEntry: BarEntry?): String {
+        val entryVal = barEntry?.y
+
+        return if (entryVal!! > 0)
+            if ((entryVal % 1.0) > 0) "$entryVal" else "${entryVal.toInt()}"
+        else
+            ""
+    }
+
+    override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+        val intVal = value.toInt()
+        return if (value < 10) "0${intVal}:00" else "${intVal}:00"
+    }
+}
 
 class Chart(chartView: BarChart, data: List<Measurement>?) {
 
@@ -15,21 +34,14 @@ class Chart(chartView: BarChart, data: List<Measurement>?) {
             entries.add(BarEntry(it.timestamp, it.value))
         }
 
-//        val exampleData = generateExampleData()
-//        exampleData.forEach { (time, value) ->
-//            entries.add(Entry(time, value))
-//        }
-
         val dataSet = BarDataSet(entries, "kW")
+
         val chartData = BarData(dataSet)
+        chartData.setValueFormatter(Formatter())
 
         chartView.data = chartData
+        chartView.xAxis.valueFormatter = Formatter()
+
         chartView.invalidate()
     }
-
-//    private fun generateExampleData(hours: Int = 24): List<Measurement> {
-//        return (0 until hours).mapTo(mutableListOf()) {
-//            Measurement(it.toFloat(), Random.nextFloat() * 20)
-//        }
-//    }
 }
